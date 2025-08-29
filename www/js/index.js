b1dcd6618ccd6d109ebf28b2f97e5f3e9eec9fb4
nsx89 <nsx89@mail.ru>
2025-08-29 18:04:55 +0300

    app open link

--- www/js/index.js
+++ www/js/index.js
@@ -30,7 +30,7 @@ let openAppWindow = () => {
             document.body.className = 'mode-online';
 
             const options = 'location=no,toolbar=no,hideurlbar=yes,hidenavigationbuttons=yes,lefttoright=yes,zoom=no';
-            siteWindow = cordova.InAppBrowser.open('https://kultura.potok8.ru/?source=app&version=100&version_app=android', '_blank', options)
+            siteWindow = cordova.InAppBrowser.open('https://kultura-doma.ru/?source=app&version=1.0.0&version_app=android', '_blank', options)
 
             siteWindow.addEventListener('loaderror', function (params) {
                 console.log('ERROR', params.message)
